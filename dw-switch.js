3e13b23294dad9384f4d3bfa378705e947998e1a
Nirmal Baldaniya <nirmalb@codetailor.in>
2019-09-10 11:01:07 +0530

    initial setup.

---
+++ dw-switch.js
@@ -0,0 +1,11 @@
+import { LitElement, css } from 'lit-element';
+import '@material/mwc-switch/mwc-switch';
+
+export class DwSwitch extends LitElement {
+  
+
+}
+
+customElements.define('dw-switch', DwSwitch);
+
+
